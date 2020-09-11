ba742bcdf6c06b5c4d6fb146e6960fd6f11654c6
Rares Vernica (remos, fedora) <vernica@remos.labs.hpicorp.net>
2020-09-11 13:18:02 +0200

    Make tests more rugged, add fitler test

--- py_pkg/tests/test_s3load.py
+++ py_pkg/tests/test_s3load.py
@@ -51,10 +51,10 @@ s3load(
 @pytest.mark.parametrize('dim_start, dim_end, chunk_size',
                          ((s, e, c)
                           for s in (-21, -13, -10)
-                          for e in (10, 15, 20)
-                          for c in (5, 10, 20)))
+                          for e in (10, 16, 19)
+                          for c in (5, 7, 10, 20)))
 def test_many_dim(scidb_con, s3_con, dim_start, dim_end, chunk_size):
-    prefix = 'many_dim_{}'.format(chunk_size)
+    prefix = 'many_dim_{}_{}_{}'.format(dim_start, dim_end, chunk_size)
     schema = '<v:int64> [i={s}:{e}:0:{c}; j=-15:14:0:{c}]'.format(
         s=dim_start, e=dim_end - 1, c=chunk_size)
 
@@ -122,7 +122,7 @@ s3load(
      for c in (5, 10, 20)))
 def test_type(scidb_con, s3_con, type_name, is_null, type_numpy, chunk_size):
     max_val = 20
-    prefix = 'test_{}_{}_{}'.format(type_name, is_null, chunk_size)
+    prefix = 'type_{}_{}_{}'.format(type_name, is_null, chunk_size)
     schema = '<v:{} {}NULL> [i=0:{}:0:{}]'.format(
         type_name, '' if is_null else 'NOT ', max_val - 1, chunk_size)
 
@@ -169,5 +169,49 @@ s3load(
         v = type_numpy(range(max_val))
 
     assert array.equals(pandas.DataFrame({'i': range(max_val), 'v': v}))
+
+    delete_prefix(s3_con, prefix)
+
+
+@pytest.mark.parametrize('dim_start, dim_end, chunk_size',
+                         ((s, e, c)
+                          for s in (-13, -11, -7)
+                          for e in (11, 13, 17)
+                          for c in (3, 7, 11)))
+def test_filter(scidb_con, s3_con, dim_start, dim_end, chunk_size):
+    prefix = 'filter_{}_{}_{}'.format(dim_start, dim_end, chunk_size)
+    schema = '<v:int64> [i={s}:{e}:0:{c}; j=-11:13:0:{c}]'.format(
+        s=dim_start, e=dim_end - 1, c=chunk_size)
+
+    # Store
+    bucket_prefix = '/'.join((base_prefix, prefix))
+    scidb_con.iquery("""
+s3save(
+  filter(build({}, i), i % 3 = 0 and i > 7),
+  bucket_name:'{}',
+  bucket_prefix:'{}')""".format(schema, bucket_name, bucket_prefix))
+
+    # Load
+    array = scidb_con.iquery("""
+s3load(
+  bucket_name:'{}',
+  bucket_prefix:'{}')""".format(bucket_name, bucket_prefix),
+                             fetch=True)
+    array = array.sort_values(by=['i', 'j']).reset_index(drop=True)
+
+    i_lst = []
+    j_lst = []
+    v_lst = []
+    for i in range(dim_start, dim_end):
+        for j in range(-11, 14):
+            if i % 3 == 0 and i > 7:
+                i_lst.append(i)
+                j_lst.append(j)
+                v_lst.append(float(i))
+
+    assert array.equals(
+        pandas.DataFrame({'i': i_lst,
+                          'j': j_lst,
+                          'v': v_lst}))
 
     delete_prefix(s3_con, prefix)
