12d35dbafe408d9fc993bea741299abb391de990
Rares Vernica (baron, fedora) <rvernica@gmail.com>
2020-11-09 20:34:02 -0800

    Update reindex and mergeindex to work with Arrow format

--- py_pkg/mergeindex.py
+++ py_pkg/mergeindex.py
@@ -2,6 +2,7 @@
 
 import boto3
 import os
+import pyarrow
 import sys
 
 
@@ -17,12 +18,30 @@ client = boto3.client('s3')
 pages = client.get_paginator('list_objects_v2').paginate(
     Bucket=bucket, Prefix='{}/index/'.format(key))
 
-index_all = []
+# index_all = []
+
+sink = pyarrow.BufferOutputStream()
+writer = None
+
 for page in pages:
     for obj_ref in page['Contents']:
         print(obj_ref['Key'])
         obj = client.get_object(Bucket=bucket, Key=obj_ref['Key'])
-        index_all.append(obj['Body'].read())
 
-index_all = b'\n'.join(index_all) + b'\n'
-client.put_object(Body=index_all, Bucket=bucket, Key='{}/index'.format(key))
+        # index_all.append(obj['Body'].read())
+
+        buf = obj['Body'].read()
+        reader = pyarrow.RecordBatchStreamReader(buf)
+        for batch in reader:
+            if writer is None:
+                writer = pyarrow.RecordBatchStreamWriter(sink, batch.schema)
+            writer.write_batch(batch)
+
+# index_all = b'\n'.join(index_all) + b'\n'
+# client.put_object(Body=index_all, Bucket=bucket, Key='{}/index'.format(key))
+
+writer.close()
+buf = sink.getvalue()
+client.put_object(Body=buf.to_pybytes(),
+                  Bucket=bucket,
+                  Key='{}/index.arrow'.format(key))
--- py_pkg/reindex.py
+++ py_pkg/reindex.py
@@ -1,7 +1,10 @@
 #!/bin/env python3
 
 import boto3
+import io
 import os
+import pyarrow
+import pyarrow.csv
 import sys
 
 
@@ -15,26 +18,47 @@ key = sys.argv[2]
 INDEX_SPLIT_SIZE = int(sys.argv[3])
 
 
-# Delete Existing Split Index
-bkt = boto3.resource('s3').Bucket(bucket)
-bkt.objects.filter(Prefix='{}/index/'.format(key)).delete()
-
-
 client = boto3.client('s3')
-obj = client.get_object(Bucket=bucket, Key='{}/index'.format(key))
+obj = client.get_object(Bucket=bucket, Key='{}/index.tsv'.format(key))
 data = obj['Body'].read()
 chunks = data.split(b'\n')
 
 while b'' in chunks:
     chunks.remove(b'')
 
-line_sz = len(chunks[0].split(b'\t'))
-split_sz = INDEX_SPLIT_SIZE // line_sz
+n_dims = len(chunks[0].split(b'\t'))
+split_sz = INDEX_SPLIT_SIZE // n_dims
+
+
+# Delete Existing Split Index
+bkt = boto3.resource('s3').Bucket(bucket)
+bkt.objects.filter(Prefix='{}/index/'.format(key)).delete()
 
 i = 0
 for split_st in range(0, len(chunks), split_sz):
-    body = b'\n'.join(chunks[split_st:(split_st+split_sz)])
+    split = chunks[split_st:(split_st+split_sz)]
+    body = b'\n'.join(split) + b'\n'
+    table = pyarrow.csv.read_csv(
+        input_file=io.BytesIO(body),
+        read_options=pyarrow.csv.ReadOptions(autogenerate_column_names=True),
+        parse_options=pyarrow.csv.ParseOptions(delimiter='\t'))
+
+    frame = table.to_pandas()
+    frame = frame * 10 + 1
+    table = pyarrow.Table.from_pandas(frame)
+
+    batches = table.to_batches()
+    sink = pyarrow.BufferOutputStream()
+    writer = pyarrow.RecordBatchStreamWriter(sink, batches[0].schema)
+    for batch in batches:
+        writer.write_batch(batch)
+    writer.close()
+    buf = sink.getvalue()
+
     split_key = '{}/index/{}'.format(key, i)
     print(split_key)
-    client.put_object(Body=body, Bucket=bucket, Key=split_key)
+    client.put_object(Body=buf.to_pybytes(), Bucket=bucket, Key=split_key)
     i += 1
+
+    # body = b'\n'.join(chunks[split_st:(split_st+split_sz)])
+    # client.put_object(Body=body, Bucket=bucket, Key=split_key)
