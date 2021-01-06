1efb60de77203c3d60a1071e88a9dc3c05d0a0b4
Rares Vernica (remos, fedora) <vernica@remos.labs.hpicorp.net>
2021-01-06 11:47:53 +0100

    Fix Pandas compatiblilty for S3 arrays as well

--- py_pkg/scidbbridge/__init__.py
+++ py_pkg/scidbbridge/__init__.py
@@ -143,7 +143,9 @@ class S3Array(_Array):
         index = table.to_pandas(split_blocks=True, self_destruct=True)
         index.sort_values(by=list(index.columns),
                           inplace=True,
-                          ignore_index=True)
+                          # ignore_index=True  # Pandas >= 1.0.0
+                          )
+        index.reset_index(inplace=True, drop=True)  # Pandas < 1.0.0
         return index
 
     def get_chunk(self, *argv):
