7a7118b161a245617ed313b0d9c2b4efe03aab52
Matt Peterson <mpeterson@paradigm4.com>
2021-05-26 16:03:45 +0000

    Basic read-only API for bridge arrays

--- r_pkg/scidbbridge/R/main.R
+++ r_pkg/scidbbridge/R/main.R
@@ -24,51 +24,11 @@
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
+library(R6)
 
-#' A Reference Class to represent an externally stored SciDB array.
-#'
-#' @field url URL to the SciDB array
-Array <- setRefClass(
-  "Array",
-  fields = c(
-    "url",
-    ".metadata"),
+#' Reads a table from S3 in arrow format and returns a dataframe
 
-  methods = list(
-    initialize = function(url) {
-      url <<- url
-      .metadata <<- NULL
-    },
-
-    metadata = function() {
-      if (is.null(.metadata)) {
-        print("Reading metadata")
-        .metadata <<- .read_metadata(url)
-      }
-      return(.metadata)
-    },
-
-    schema = function() {
-      return(metadata()$schema)
-    },
-
-    index = function() {
-      return(.read_index(url), .metadata)
-    }
-  )
-)
-
-.scidbbridge <- new.env()
-
-.s3 = function() {
-  if (!exists("s3", .scidbbridge)) {
-    print("Create S3 client")
-    assign("s3", paws::s3(), .scidbbridge)
-  }
-  return(.scidbbridge$s3)
-}
-
-.parse_url = function(url) {
+parse_url = function(url) {
   if (grepl(url, " ")) stop(paste0("Invalid URL '", url, "' - contains spaces"))
 
   url_parts <- scan(
@@ -88,73 +48,212 @@ Array <- setRefClass(
               "path"   = path))
 }
 
-.read_metadata = function(url) {
-  parts <- .parse_url(url)
-  parts$path <- paste0(parts$path, "/metadata")
+#' A class representing an externally stored SciDB array
+Array <- R6Class(
+  "Array",
+  clone = F, portable = F,
+  public = list(
+    url = NA,
+    initialize = function(url) {
+      self$url = url
+    },
+    metadata = function() {
+      if(is.null(private$.metadata)) {
+        private$.metadata = private$read_metadata()
+      }
 
-  if (parts$scheme == "s3") {
-    req <- .s3()$get_object(Bucket = parts$bucket, Key = parts$path)
-    df <- utils::read.table(text = rawToChar(req$Body), sep = "\t")
-  }
-  else if (parts$scheme == "file")
-    df <- utils::read.table(parts$path, sep = "\t")
+      return(private$.metadata)
+    },
+    read_index = function() {
+      parts <- parse_url(self$url)
 
-  schema <- split(df$V2, df$V1)
-  return(schema)
-}
+      keys = list()
 
-# Extracts a table from a bucket/key combo
-.read_table = function(bucket, key, compression = FALSE) {
-  s3_obj = s3$get_object(Bucket = "bmsrd-ngs-64223", Key = "incoming/VariantWarehouse/bridge-test/chr22/index/0")
+      if (parts$scheme == "s3") {
+        # Need to get all of the index arrays
+        truncated = TRUE
+        contToken = character(0)
 
-  # Set the compression if we need to (only using gzip for now)
-  if(compression) {
-    stream = arrow::CompressedInputStream$create(arrow::BufferReader$create(s3_obj$Body), arrow::Codec$create("gzip"))
-  } else {
-    stream = arrow::CompressedInputStream$create(arrow::BufferReader$create(s3_obj$Body))
-  }
+        # Get the set of keys to read
+        while(truncated) {
+          objects = private$s3$list_objects_v2(Bucket = parts$bucket, Prefix = paste0(parts$path, "/index/"))
 
-  reader = arrow::RecordBatchStreamReader$create(stream)
-}
+          truncated = objects$IsTruncated
+          contToken = objects$NextContinuationToken
 
-.read_index = function(url, metadata) {
+          keys = append(keys, lapply(objects$Contents, function(x) unlist(x[c('Key')])))
+        }
 
-  parts <- .parse_url(url)
+        frames = lapply(keys, function(x) read_table_s3(bucket = parts$bucket, key = x))
+        return(do.call(rbind, frames))
+      } else if (parts$scheme == "file") {
+        stop("Local files are yet to be implemented.")
+      }
+    },
+    get_chunk = function(...) {
 
-  keys = list()
+      chunk_file = private$get_chunk_prefix(...)
+      parts = parse_url(self$url)
 
-  if (parts$scheme == "s3") {
-    # Need to get all of the index arrays
-    truncated = TRUE
-    contToken = character(0)
+      chunk_key = glue::glue("{parts$path}/chunks/{chunk_file}")
 
-    # Get the set of keys to read
-    while(truncated) {
-      objects = s3$list_objects_v2(Bucket = parts$bucket, Prefix = parts$path, ContinuationToken = contToken)
-      truncated = objects$IsTruncated
-      contToken = objects$NextContinuationToken
+      if(parts$scheme == "s3") {
+        gzipped = self$metadata()$compression == "gzip"
+        return(private$read_table_s3(bucket = parts$bucket, key = chunk_key, gzipped = gzipped))
+      } else {
+        stop("Local files not yet implemented.")
+      }
 
-      append(keys, lapply(objects$Contents, function(x) unlist(x[c('Key')])))
+    },
+    schema = function() {
+      return(self$metadata()$schema)
     }
+  ),
+  private = list(
+    .metadata = NULL,
+    s3 = paws::s3(),
+    read_metadata = function() {
+      parts <- parse_url(self$url)
+      parts$path <- paste0(parts$path, "/metadata")
 
-    return(rbind(lapply(function(x) .read_table_s3(bucket, x), keys)))
+      if (parts$scheme == "s3") {
+        req <- private$s3$get_object(Bucket = parts$bucket, Key = parts$path)
+        df <- utils::read.table(text = rawToChar(req$Body), sep = "\t")
+      }
+      else if (parts$scheme == "file")
+        df <- utils::read.table(parts$path, sep = "\t")
 
-  } else if (parts$scheme == "file") {
-    stop("Local files are yet to be implemented.")
-  }
+      schema <- split(df$V2, df$V1)
+      return(schema)
+    },
+    read_table_s3 = function(bucket, key, gzipped = TRUE) {
 
-}
+      s3_obj = private$s3$get_object(Bucket = bucket, Key = key)
 
-# Read in a table in Arrow format, returning a data.frame
-.read_table_s3 <- function(bucket, key, gzipped = FALSE) {
+      if(gzipped) {
+        stream = arrow::CompressedInputStream$create(arrow::BufferReader$create(s3_obj$Body), arrow::Codec$create("gzip"))
+      } else {
+        stream = arrow::BufferReader$create(s3_obj$Body)
+      }
 
-  s3_obj = .s3$get_object(Bucket = bucket, Key = key)
-  if(gzipped) {
-    stream = arrow::CompressedInputStream(arrow::BufferReader$create(s3_obj$body), arrow::Codec$create("gzip"))
-  } else {
-    stream = arrow::InputStream(arrow::BufferReader$create(s3_obj$body))
-  }
+      reader = arrow::RecordBatchStreamReader$create(stream)
+      return(as.data.frame(reader$read_table()))
+    },
 
-  reader = arrow::RecordBatchStreamReader$create(stream)
-  return(as.data.frame(reader$read_table()))
-}
+    get_chunk_prefix = function(...) {
+      # Get the coordinates
+      coords = list(...)
+
+      schema_dims = private$get_dimensions()
+
+      all_match = all(names(coords) == schema_dims$name)
+      stopifnot(names(coords) == schema_dims$name)
+
+      dims = schema_dims
+      dims$parts = unlist(coords)
+
+      # TODO: The input needs to be checked to ensure it is valid.
+      dims$parts = dims$parts - as.integer(dims$start)
+      dims$parts = floor(dims$parts/as.integer(dims$chunk))
+
+      chunk_str = paste(c("c", dims$parts), sep="", collapse="_")
+      return(chunk_str)
+    },
+
+    #' Gets the dimensions for a bridge array as a data frame
+    #'
+    #' Taken from unexported methods of the SciDBR package
+    get_dimensions = function() {
+
+      # Get the schema here for the object
+      x = self$metadata()$schema
+
+      x = gsub("\\t", " ", x)
+      x = gsub("\\n", " ", x)
+      tokenize = function(s, token)
+      {
+        x = strsplit(s, token)[[1]]
+        x = as.vector(rbind(x, rep(token, length(x))))
+        x[- length(x)]
+      }
+
+      diagram = function(tokens, labels=c())
+      {
+        if(length(tokens) == 0) return(labels)
+        last = tail(labels, 1)
+        prev = tail(labels, 2)[1]
+        if(is.null(last))              labels = c(labels, "name")
+        else if(tokens[1] == "=")      labels = c(labels, "equals")
+        else if(tokens[1] == ";")      labels = c(labels, "semicolon")
+        else if(tokens[1] == ":")      labels = c(labels, "colon")
+        else if(tokens[1] == ",")      labels = c(labels, "comma")
+        else
+        {
+          if(last == "semicolon")      labels = c(labels, "name")
+          else if(last == "equals")    labels = c(labels, "start")
+          else if(last == "colon")
+          {
+            if(is.null(prev))          stop("invalid : character")
+            else if(prev == "start")   labels = c(labels, "end")
+            else if(prev == "end")     labels = c(labels, "overlap")
+            else if(prev == "overlap") labels = c(labels, "chunk")
+          }
+          else if(last == "comma")
+          {
+            if(is.null(prev))          stop("invalid , character")
+            else if(prev == "name")    labels = c(labels, "name")
+            else if(prev == "start")   labels = c(labels, "end")
+            else if(prev == "end")     labels = c(labels, "chunk")
+            else if(prev == "chunk")   labels = c(labels, "overlap")
+            else if(prev == "overlap") labels = c(labels, "name")
+          }
+        }
+        diagram(tokens[-1], labels)
+      }
+      form = function(x)
+      {
+        c(name=x["name"], start=x["start"], end=x["end"], chunk=x["chunk"], overlap=x["overlap"])
+      }
+
+      s = tryCatch(gsub("]", "", strsplit(x, "\\[")[[1]][[2]]), error=function(e) NULL)
+      if(is.null(s) || nchar(s) == 0) return(NULL)
+      tokens = Reduce(c, lapply(Reduce(c, lapply(Reduce(c, lapply(tokenize(s, "="), tokenize, ":")), tokenize, ";")), tokenize, ","))
+      names(tokens) = diagram(tokens)
+      tokens[!(names(tokens) %in% c("equals", "colon", "semicolon", "comma"))]
+      i = which(names(tokens) %in% "name")
+      j = c((i - 1)[-1], length(tokens))
+      ans = Reduce(rbind, lapply(1:length(i), function(k) form(tokens[i[k]:j[k]])))
+      if(length(i) == 1) {
+        ans = data.frame(as.list(ans), stringsAsFactors=FALSE, row.names=NULL)
+      } else ans = data.frame(ans, stringsAsFactors=FALSE, row.names=c())
+      names(ans) = c("name", "start", "end", "chunk", "overlap")
+      ans$name = gsub(" ", "", ans$name)
+      return(ans)
+    },
+
+    #' Internal function for processing SciDB attribute schema
+    #' @param x a scidb object or schema string
+    #' @return a data frame with parsed attribute data
+    .attsplitter = function(x)
+    {
+      if (is.character(x)) s = x
+      else
+      {
+        if (!(inherits(x, "scidb"))) return(NULL)
+        s = schema(x)
+      }
+      s = gsub("\\t", " ", s)
+      s = gsub("\\n", " ", s)
+      s = gsub("default[^,]*", "", s, ignore.case=TRUE)
+      s = strsplit(strsplit(strsplit(strsplit(s, ">")[[1]][1], "<")[[1]][2], ",")[[1]], ":")
+      # SciDB schema syntax changed in 15.12
+      null = if (at_least(attr(x@meta$db, "connection")$scidb.version, "15.12"))
+        ! grepl("NOT NULL", s)
+      else grepl(" NULL", s)
+      type = gsub(" ", "", gsub("null", "", gsub("not null", "", gsub("compression '.*'", "", vapply(s, function(x) x[2], ""), ignore.case=TRUE), ignore.case=TRUE), ignore.case=TRUE))
+      data.frame(name=gsub("[ \\\t\\\n]", "", vapply(s, function(x) x[1], "")),
+                 type=type,
+                 nullable=null, stringsAsFactors=FALSE)
+    }
+  )
+)
