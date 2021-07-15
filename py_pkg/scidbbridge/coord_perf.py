ff0540a300d20555182a80f439cf9c734d2acd04
Rares Vernica <rvernica@gmail.com>
2021-07-15 03:21:58 +0000

    Re-write to work on columns and use flexible columns

--- py_pkg/scidbbridge/coord_perf.py
+++ py_pkg/scidbbridge/coord_perf.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy
 import pandas
 import pyarrow
@@ -27,48 +28,67 @@ from coord import *
 # ---
 # Read input file
 # ---
-def read_file(file_name_in):
-    names = ('d0',                  # 0
-             'd1',                  # 1
-             'd0_o',                # 2
-             'd1_o')                # 3
-    if '_pos.' in file_name_in:
-        names = names + ('pos',
-                         'prev_d0_o',
-                         'prev_d1_o',
-                         'prev_pos',
-                         'delta')
-    dtype = dict((k, numpy.int64) for k in names)
+def read_file(file_name, n_dims, attr_names=(), attr_types=()):
+    dim_names = tuple('d{}'.format(i) for i in range(n_dims))
+    dim_dtype = dict((k, numpy.int64) for k in dim_names)
+
+    names = tuple(itertools.chain(dim_names, attr_names))
+    dtypes = dict(itertools.chain(dim_dtype.items(),
+                                  ((name, dtype) 
+                                   for (name, dtype) in zip(attr_names,
+                                                            attr_types))))
+
+    return pandas.read_csv(file_name, names=names, dtype=dtypes)
 
 
-    return pandas.read_csv(file_name_in, names=names, dtype=dtype)
+# ---
+# Write output file
+# ---
+def write_dile(data, file_name):
+    data.to_csv(file_name, header=False, index=False)
 
 
 # ---
 # Add pos column
 # ---
-def add_pos(file_name_in, chunk_size):
-    data = read_file(file_name_in)
-    file_name_out = file_name_in[:-4] + '_pos' + file_name_in[-4:]
+def compute_delta(data, origins, chunk_sizes):
+    n_dims = len(origins)
+    n_atts = len(data.columns) - n_dims
+    
+    dim_names = tuple('d{}'.format(i) for i in range(n_dims))
+    origin_names = tuple('o{}'.format(i) for i in range(n_dims))
+    prev_origin_names = tuple('prev_o{}'.format(i) for i in range(n_dims))
+    new_dim_names = tuple('new_d{}'.format(i) for i in range(n_dims))
+
+    # Compute chunk origin
+    for i in range(n_dims):
+        data[origin_names[i]] = ((data[dim_names[i]] - origins[i]) 
+                                 // chunk_sizes[i]
+                                 * chunk_sizes[i]
+                                 + origins[i])
 
     # Compute pos
-    data['pos'] = data.apply(
-        lambda row: coord2pos(row[:2], row[2:4], chunk_size),
-        axis=1)                            # 4
+    data['pos'] = coord2pos_all(data,
+                                dim_names,
+                                origin_names,
+                                chunk_sizes)
 
     # Shift and assign prev_* columns
     data_prev = data.shift(1, fill_value=0)
-    data['prev_d0_o'] = data_prev['d0_o']  # 5
-    data['prev_d1_o'] = data_prev['d1_o']  # 6
-    data['prev_pos'] = data_prev['pos']    # 7
 
-    def pos2delta(row):
-        if row[2:4].tolist() == row[5:7].tolist():  # Same chunk
-            return row['pos'] - row['prev_pos']
-        return row['pos']
+    # Set previous values (for delta compuation)
+    for i in range(n_dims):
+        data[prev_origin_names[i]] = data_prev[origin_names[i]]
+    data['prev_pos'] = data_prev['pos']
 
-    # Compute delta
-    data['delta'] = data.apply(pos2delta, axis=1)
+    # Set new chunk markers
+    data['new_chunk'] = False
+    for (name, prev_name) in zip(origin_names, prev_origin_names):
+        data['new_chunk'] |= (data[name] != data[prev_name])
+
+    # if new_chunk then delta = pos
+    # else delta = pos - prev_pos
+    data['delta'] = data['pos'] - ~data['new_chunk'] * data['prev_pos']
 
     # Reset first delta due to shift
     data.loc[0, 'delta'] = data.loc[0, 'pos']
@@ -79,30 +99,38 @@ def add_pos(file_name_in, chunk_size):
             "Something's off | Negative delta found\n{}".format(
                 data.loc[data['delta'] < 0]))
 
-    # Save to file
-    data.to_csv(file_name_out, header=False, index=False)
+    # Compute new pos
+    # if new_chunk pos = delta
+    # else pos = prev_pos + delta
+    data['new_pos'] = ~data['new_chunk'] * data['prev_pos'] + data['delta']
 
-    # Do reverse computation and verify results
-    def delta2pos(row):
-        if row[2:4].tolist() == row[5:7].tolist():  # Same chunk
-            return row['prev_pos'] + row['delta']
-        return row['delta']
+    # Compute new coords
+    data = data.join(
+        pos2coord_all(data, 
+                      'new_pos',
+                      origin_names,
+                      chunk_sizes,
+                      new_dim_names))
 
-    data['new_pos'] = data.apply(delta2pos, axis=1)
-    data[['new_d0', 'new_d1']] = data.apply(
-        lambda row: pos2coord(row['new_pos'], row[2:4], chunk_size),
-        axis=1,
-        result_type='expand')
+    # Verify that new pos and coords match
+    for (name, new_name) in zip(dim_names, new_dim_names):
+        if not all(data[name] == data[new_name]):
+            raise Exception(
+                ("Something's off | Computed {} " +
+                 "coords don't match\n{}").format(
+                     name,
+                     data.loc[data[name] != data[new_name]]))
+        if not all(data[name] == data[new_name]):
+            raise Exception(
+                ("Something's off | Computed {} " +
+                 "coords don't match\n{}").format(
+                     name,
+                     data.loc[data[name] != data[new_name]]))
 
-    if not all(data['d0'] == data['new_d0']):
-        raise Exception(
-            "Something's off | Computed d0 coords don't match\n{}".format(
-                data.loc[data['d0'] != data['new_d0']]))
-
-    if not all(data['d1'] == data['new_d1']):
-        raise Exception(
-            "Something's off | Computed d1 coords don't match\n{}".format(
-                data.loc[data['d1'] != data['new_d1']]))
+    # Delete new pos and coords
+    del data['new_pos']
+    for name in new_dim_names:
+        del data[name]
 
 
 # ---
@@ -128,14 +156,3 @@ def write_arrow(file_name, dataframe, compression='lz4'):
 # %timeit coord2pos_all(data, ('d0', 'd1'), ('d0_o', 'd1_o'), chunk_size)
 # %timeit pos2coord_all(
 #     data, 'pos', ('d0_o', 'd1_o'), chunk_size, ('d0', 'd1'))
-
-
-
-# ---
-# Main: Add pos column
-# ---
-if __name__ == '__main__':
-    file_name_in = sys.argv[1]
-    chunk_size = tuple(map(int, sys.argv[2].split(',')))
-    if '_pos.' not in file_name_in:
-        add_pos(file_name_in, chunk_size)
